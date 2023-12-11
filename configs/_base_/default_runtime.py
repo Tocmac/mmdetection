5c9d8f54595400df4b69e7f72d2945d2e329ea12
xuwang <xu.wangxu@foxmail.com>
2023-12-11 19:16:16 +0800

    add local vis

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -15,7 +15,8 @@ env_cfg = dict(
 )
 
 # vis_backends = [dict(type='LocalVisBackend')]
-vis_backends = [dict(type='TensorboardVisBackend')]
+vis_backends = [dict(type='TensorboardVisBackend'),
+                dict(type='LocalVisBackend')]
 visualizer = dict(
     type='DetLocalVisualizer', vis_backends=vis_backends, name='visualizer')
 log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
